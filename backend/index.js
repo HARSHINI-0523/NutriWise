2d48944640b9f721a565ea6e4581db4d0908c2b1
Rupa <roopasri.0812@gmail.com>
2025-10-01 18:34:01 +0530

    userdetailsform

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
-// index.js
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
 dotenv.config();
 const app = express();
-PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
-//Import routes
+// Import routes
 const authRouter = require("./routes/auth");
-const reportRoutes = require('./routes/reportRoutes');
+const reportRoutes = require("./routes/reportRoutes");
+const userDetailsRouter = require("./routes/userDetails");
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-//Use routes
+// Use routes
 app.use("/api/auth", authRouter);
-app.use('/api/reports', reportRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/user-details", userDetailsRouter);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
